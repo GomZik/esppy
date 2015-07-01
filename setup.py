586dec19f47b9dfbdc98c0801f5d83a9c410b20b
Aliaksiej Homza <aliaksei.homza@gmail.com>
2015-07-01 20:48:49 +0300

    Add setup py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from esppy.version import get_version
+
+
+setup(
+    name='esppy',
+    version=get_version(),
+    author='Aliaksiej Homza',
+    author_email='aliakei.homza@gmail.com',
+    description='Erlang style process for python',
+    license='MIT',
+    packages=['esppy'],
+    long_description=''
+)
